bf00a7fe6469766228228e857b59f36b4f7b3594
singularitti <singularitti@outlook.com>
2023-02-13 02:52:21 -0500

    Add `_tensor` in src/invariants.jl

--- src/invariants.jl
+++ src/invariants.jl
@@ -8,17 +8,19 @@ principal_values(x::Union{TensorStress,TensorStrain}) = _eigen(x).values
 
 principal_axes(x::Union{TensorStress,TensorStrain}) = _eigen(x).vectors
 
-principal_invariants(x::Union{TensorStress,TensorStrain}) = stress_invariants(parent(x))
+principal_invariants(x::Union{TensorStress,TensorStrain}) = stress_invariants(_tensor(x))
 
 main_invariants(x::Union{TensorStress,TensorStrain}) =
-    deviatoric_stress_invariants(parent(x))
+    deviatoric_stress_invariants(_tensor(x))
 
 for T in (:TensorStress, :TensorStrain)
     @eval begin
-        hydrostatic(x::$T) = $T(vol(SymmetricSecondOrderTensor{3}(float(x))))
-        deviatoric(x::$T) = $T(dev(SymmetricSecondOrderTensor{3}(float(x))))
+        hydrostatic(x::$T) = $T(vol(_tensor(x)))
+        deviatoric(x::$T) = $T(dev(_tensor(x)))
     end
 end
+
+_tensor(x) = SymmetricSecondOrderTensor{3}(float(x))
 
 function _eigen(x)
     x′ = @. x / oneunit(x)  # Dimensionless
