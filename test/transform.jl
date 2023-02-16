d9f6a1c15222a60d048e6bfcb169de074b097372
singularitti <singularitti@outlook.com>
2023-02-16 07:01:22 -0500

    Add "Test homework 3 question 1" & "Test homework 3 question 2"

--- test/transform.jl
+++ test/transform.jl
@@ -20,6 +20,44 @@
     end
 end
 
+@testset "Test homework 3 question 1" begin
+    σ = TensorStress([
+        0 0 0
+        0 245 0
+        0 0 0
+    ])
+    Q = [
+        1 1 0
+        -1 1 0
+        0 0 √2
+    ] / √2
+    @test rotate_axes(σ, Q)[1, 2] ≈ sin(2 * pi / 4) / 2 * σ[2, 2]
+end
+
+@testset "Test homework 3 question 2" begin
+    σ = TensorStress([
+        300 100 0
+        100 100 0
+        0 0 0
+    ])
+    Q = θ -> [
+        cos(θ) sin(θ) 0
+        -sin(θ) cos(θ) 0
+        0 0 1
+    ]
+    @testset "Determine the principal stresses" begin
+        @test rotate_axes(σ, Q(pi / 8)) ≈ TensorStress([
+            200+100 * √2 0 0
+            0 200-100 * √2 0
+            0 0 0
+        ])
+    end
+    @testset "Determine the maximum in-plane shear" begin
+        @test rotate_axes(σ, Q(-pi / 8))[1, 2] == 100 * √2
+        @test rotate_axes(σ, Q(3pi / 8))[1, 2] == -100 * √2
+    end
+end
+
 @testset "Test `rotate` of tensor strains" begin
     ε = TensorStrain([
         0.5 0.3 0.2
