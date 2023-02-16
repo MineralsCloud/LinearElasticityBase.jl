012e5f22d1f4394510168847eb1967981cfec2f2
singularitti <singularitti@outlook.com>
2023-02-15 22:48:16 -0500

    Merge methods in src/invariants.jl

--- src/invariants.jl
+++ src/invariants.jl
@@ -4,17 +4,12 @@ using Tensorial: stress_invariants, deviatoric_stress_invariants, vol, dev
 export principal_values,
     principal_axes, principal_invariants, main_invariants, hydrostatic, deviatoric
 
-principal_values(x::Union{TensorStress,TensorStrain}) = _eigen(x).values
-
-principal_axes(x::Union{TensorStress,TensorStrain}) = _eigen(x).vectors
-
-principal_invariants(x::Union{TensorStress,TensorStrain}) = stress_invariants(_tensor(x))
-
-main_invariants(x::Union{TensorStress,TensorStrain}) =
-    deviatoric_stress_invariants(_tensor(x))
-
 for T in (:TensorStress, :TensorStrain)
     @eval begin
+        principal_values(x::$T) = _eigen(x).values
+        principal_axes(x::$T) = _eigen(x).vectors
+        principal_invariants(x::$T) = stress_invariants(_tensor(x))
+        main_invariants(x::$T) = deviatoric_stress_invariants(_tensor(x))
         hydrostatic(x::$T) = $T(vol(_tensor(x)))
         deviatoric(x::$T) = $T(dev(_tensor(x)))
     end
